a546bb04fce17c4f9506dad71f853b9828abde6b
Arm <41083078+TheArmKing@users.noreply.github.com>
2019-06-20 19:22:01 -0700

    Update UnityVersionAndroid.sh

--- UnityVersionAndroid.sh
+++ UnityVersionAndroid.sh
@@ -20,6 +20,7 @@ zippidy(){
   rm "/storage/emulated/0/j2vj2nl1moi3p1p"
 }
 if [ "$1" == "" ]; then echo "File Path" && read pat; else pat="$1"; fi
+if [ ! -f "$pat" ]; then thdpl "File doesn't exist" && exit; fi
 exion="$(echo "$pat" | tail -c 5)"
 if [ "$exion" == ".apk" ]; then
   thdpl "APK Detected"
